294dc7df35432cc54511a7351e77cb0b24e1441e
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-29 23:24:58 -0500

    added weather icons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 let searchButton = document.querySelector('button');
-let cityName = ('');
+let cityName = [''];
 let cityN = document.getElementById('searchTerm');
 let recentSearchEl = document.getElementById('recentSearch');
 let reset = document.getElementById('reset');
@@ -8,9 +8,8 @@ let tempValue = document.querySelector('.search-temp-today');
 let descValue = document.querySelector('.search-description-today');
 let windValue = document.querySelector('.search-wind-today');
 
-
-//set cityname to local storage
-//localStorage.setItem('cityName', JSON.stringify(cityName));
+//make localstorage array
+//let recentSearch = JSON.parse(localStorage.getItem('recentSearch')) || [];
 
 //add event listener for button click function
 searchButton.addEventListener('click', function () {
@@ -26,10 +25,11 @@ searchButton.addEventListener('click', function () {
     let descValue = data['list'][3]['weather'][0]['description'];
     let windValue = data['list'][3]['wind']['speed'];
     let dateValue = data['list'][3]['dt_txt'];
+    let iconValue = data['list'][3]['weather'][0]['icon'];
 
     document.querySelector('.search-name-day1').innerHTML = nameValue;
     document.querySelector('.search-temp-day1').innerHTML = 'feels like: ' + tempValue + '°C';
-    document.querySelector('.search-description-day1').innerHTML = descValue;
+    document.querySelector('.search-description-day1').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
     document.querySelector('.search-wind-day1').innerHTML = 'wind speed: ' + windValue + 'km/h';
     document.querySelector('.search-date-day1').innerHTML = dateValue;
     });
@@ -44,10 +44,11 @@ searchButton.addEventListener('click', function () {
       let descValue = data['list'][11]['weather'][0]['description'];
       let windValue = data['list'][11]['wind']['speed'];
       let dateValue = data['list'][11]['dt_txt'];
+      let iconValue = data['list'][11]['weather'][0]['icon'];
   
       document.querySelector('.search-name-day2').innerHTML = nameValue;
       document.querySelector('.search-temp-day2').innerHTML = 'feels like: ' + tempValue + '°C';
-      document.querySelector('.search-description-day2').innerHTML = descValue;
+      document.querySelector('.search-description-day2').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
       document.querySelector('.search-wind-day2').innerHTML = 'wind speed: ' + windValue + 'km/h';
       document.querySelector('.search-date-day2').innerHTML = dateValue;
       });
@@ -62,10 +63,11 @@ searchButton.addEventListener('click', function () {
       let descValue = data['list'][19]['weather'][0]['description'];
       let windValue = data['list'][19]['wind']['speed'];
       let dateValue = data['list'][19]['dt_txt'];
+      let iconValue = data['list'][19]['weather'][0]['icon'];
   
       document.querySelector('.search-name-day3').innerHTML = nameValue;
       document.querySelector('.search-temp-day3').innerHTML = 'feels like: ' + tempValue + '°C';
-      document.querySelector('.search-description-day3').innerHTML = descValue;
+      document.querySelector('.search-description-day3').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
       document.querySelector('.search-wind-day3').innerHTML = 'wind speed: ' + windValue + 'km/h';
       document.querySelector('.search-date-day3').innerHTML = dateValue;
       });
@@ -80,10 +82,11 @@ searchButton.addEventListener('click', function () {
       let descValue = data['list'][27]['weather'][0]['description'];
       let windValue = data['list'][27]['wind']['speed'];
       let dateValue = data['list'][27]['dt_txt'];
+      let iconValue = data['list'][27]['weather'][0]['icon'];
   
       document.querySelector('.search-name-day4').innerHTML = nameValue;
       document.querySelector('.search-temp-day4').innerHTML = 'feels like: ' + tempValue + '°C';
-      document.querySelector('.search-description-day4').innerHTML = descValue;
+      document.querySelector('.search-description-day4').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
       document.querySelector('.search-wind-day4').innerHTML = 'wind speed: ' + windValue + 'km/h';
       document.querySelector('.search-date-day4').innerHTML = dateValue;
       });
@@ -98,10 +101,11 @@ searchButton.addEventListener('click', function () {
       let descValue = data['list'][35]['weather'][0]['description'];
       let windValue = data['list'][35]['wind']['speed'];
       let dateValue = data['list'][35]['dt_txt'];
+      let iconValue = data['list'][35]['weather'][0]['icon'];
   
       document.querySelector('.search-name-day5').innerHTML = nameValue;
       document.querySelector('.search-temp-day5').innerHTML = 'feels like: ' + tempValue + '°C';
-      document.querySelector('.search-description-day5').innerHTML = descValue;
+      document.querySelector('.search-description-day5').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
       document.querySelector('.search-wind-day5').innerHTML = 'wind speed: ' + windValue + 'km/h';
       document.querySelector('.search-date-day5').innerHTML = dateValue;
       });
@@ -115,13 +119,13 @@ searchButton.addEventListener('click', function () {
         let tempValue = data['main']['temp'];
         let descValue = data['weather'][0]['description'];
         let windValue = data['wind']['speed'];
-        //let dateValue = data['list']['dt_txt'];
+        let iconValue = data['weather'][0]['icon'];
     
         document.querySelector('.search-name-today').innerHTML = nameValue;
         document.querySelector('.search-temp-today').innerHTML = 'feels like: ' + tempValue + '°C';
-        document.querySelector('.search-description-today').innerHTML = descValue;
+        document.querySelector('.search-description-today').innerHTML = descValue + ' ' + '<img src="http://openweathermap.org/img/w/' + iconValue + '.png" alt="weather icon">';
         document.querySelector('.search-wind-today').innerHTML = 'wind speed: ' + windValue + 'km/h';
-        //document.querySelector('.search-date-day5').innerHTML = dateValue;
+       
         });
   });
 
@@ -130,110 +134,18 @@ searchButton.addEventListener('click', function () {
 function getCityName() {
   
   cityName = cityN.value;
-  console.log(cityName);
-  //add cityName variable to local storage
-  localStorage.setItem('cityName', JSON.stringify(cityName));
+  console.log[cityName];
+  function addCity() {
+    // Parse any JSON previously stored in cityName
+     cityName = JSON.parse(localStorage.getItem["cityName"]) || [];
+    if(cityName == null) cityName = [];
+    // Add new data to cityName array
+    cityName.push[cityName];
+    // Save cityName back to local storage
+    localStorage.setItem("cityName", JSON.stringify[cityName]);
+  }
 let city = document.createElement('h3');
 city.textContent = cityName;
 recentSearchEl.appendChild(city);
-getApi();
 }
 
-//RunApi weather results from cityName
-function getApi() {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4d14b0d70a4eb05b8ec74e11ac993cd4`, {
-    method:'GET',
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      
-    })
-}
-
-
-/*//add event listener for reset button
-reset.addEventListener('click', function () {
-  localStorage.clear('cityName');
-//remove created elements
-  recentSearchEl.removeChild(city);
-  
-})*/
-
-
-
-
-function getApi () {
-
-}
-
-//button click to submit search
-
-//function on click input to search term/localstorage
-/*var searchFormEl = document.querySelector('#search-form');
-
- .then(function (data) {
-    console.log(data);
-    let nameValue = data['city']['name'];
-    let tempValue = data['list'][8]['main']['temp'];
-    let descValue = data['list'][8]['weather'][8]['description'];
-    let windValue = data['list'][8]['wind']['speed'];
-
-    document.querySelector('.search-name-day1').innerHTML = nameValue;
-    document.querySelector('.search-temp-day1').innerHTML = 'Currently feels like: ' + tempValue + '°C';
-    document.querySelector('.search-description-day1').innerHTML = descValue;
-    document.querySelector('.search-wind-day1').innerHTML = 'Current wind speed: ' + windValue + 'km/h';
-    })
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#cityN').value;
-  
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-
-
-function getApi() {
-    console.log('hey');
-    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={'+ cityName +'}&appid={4d14b0d70a4eb05b8ec74e11ac993cd4}';
-    console.log(cityName);
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //Using console.log to examine the data
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          //Creating a h3 element and a p element
-          var userName = document.createElement('h3');
-          var userUrl = document.createElement('p');
-  
-          //Setting the text of the h3 element and p element.
-          userName.textContent = data[i].login;
-          userUrl.textContent = data[i].url;
-  
-          //Appending the dynamically generated html to the div associated with the id="users"
-          //Append will attach the element as the bottom most child.
-          usersContainer.append(userName);
-          usersContainer.append(userUrl);
-        }
-      });
-  }
-  searchButton.addEventListener('submit', getApi);
-
-  // add event listener for button click function
-*/
